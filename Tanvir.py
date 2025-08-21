f2132adfdb2cebfe4331d9e61b3f961874fc9b7c
TANVIR5-404 <100285314+TANVIR5-404@users.noreply.github.com>
2025-08-21 10:03:56 +0600

    Up Tanvir.py

--- Tanvir.py
+++ Tanvir.py
@@ -1,41 +1,4 @@
 
-#---------------------[MODULES ]---------------------#
-from bs4 import BeautifulSoup as sop
-from concurrent.futures import ThreadPoolExecutor as ThreadPool
-import os
-import random
-import requests,bs4,json,sys,random,datetime,time,re,subprocess,platform,struct
-from bs4 import BeautifulSoup as sop
-from concurrent.futures import ThreadPoolExecutor as tred
-import base64
-import os,sys,time,json,random,re,string,platform,base64
-import requests
-from concurrent.futures import ThreadPoolExecutor as ThreadPool
-import mechanize
-from requests.exceptions import ConnectionError
-import string
-try:
-    import requests
-except ImportError:
-    print('\n [✓] installing requests !...\n')
-    os.system('pip install requests')
-
-try:
-    import concurrent.futures
-except ImportError:
-    print('\n [✓] installing futures !...\n')
-    os.system('pip install futures')
-try:
-    import bs4
-except ImportError:
-    print('\n [✓] installing bs4 !...\n')
-    os.system('pip install bs4')
-    os.system('git pull')
-    os.system('pkg install curl')
-import requests, os, re, bs4,platform, sys, json, time, random, datetime, subprocess, threading, itertools,base64,uuid,zlib
-from concurrent.futures import ThreadPoolExecutor as ahmadAXI
-from datetime import datetime
-from bs4 import BeautifulSoup
 
 
 ct = datetime.now()
@@ -348,3 +311,4 @@ def b(uid,pwx,tl):
     except:
         pass
 Main()
+
